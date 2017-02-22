dc1c9d0d161e110345ee992e37309d76e366a869
Marc Tim Thiemann <marc.t.thiemann@gmail.com>
2017-02-21 21:58:31 -0800

    feat: Add one-vs-all classifier training (ex3)

--- octave/ex3/oneVsAll.m
+++ octave/ex3/oneVsAll.m
@@ -50,15 +50,13 @@ X = [ones(m, 1) X];
 %
 
 
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
-
-
+for i=1:num_labels
+  c = ones(m, 1) * i;
+  [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+  all_theta(i, :) = theta;
 
 % =========================================================================
 
